837f38c78a38eb399791fa82d24419d965fbb78e
arif cahyo prasetyo <51110242+mascahyo1@users.noreply.github.com>
2023-03-24 16:27:42 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'NuxtAppName',
+      name: 'frontarfan',
       port: 3000,
       exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
+      instances: '1', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
