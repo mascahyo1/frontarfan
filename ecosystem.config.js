e517bada8832f5306421024dd335a5301e5bd051
arifcahyo09 <arif.0009@students.amikom.ac.id>
2023-04-14 11:28:44 +0700

    change 3002 port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'frontarfan_alt',
-      port: 3000,
+      port: 3002,
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
